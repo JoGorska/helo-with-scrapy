a78dd3a43eb2708c2bfdc9723ec71da87c60b041
Joanna Gorska <gorska919@gmail.com>
2022-02-09 23:26:57 +0000

    Add wikipedia crawler

--- ietf_scraper/ietf_scraper/spiders/wikipedia.py
+++ ietf_scraper/ietf_scraper/spiders/wikipedia.py
@@ -5,7 +5,13 @@ from scrapy.linkextractors import LinkExtractor
 class WikipediaSpider(CrawlSpider):
     name = 'wikipedia'
     allowed_domains = ['en.wikipedia.org']
-    start_urls = ['http://en.wikipedia.org/']
+    start_urls = ['https://en.wikipedia.org/wiki/Kevin_Bacon']
 
-    def parse(self, response):
-        pass
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
+
+    def parse_info(self, response):
+        return {
+            'title': response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()').get(),
+            'url': response.url,
+            'last_edited': response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+        }
